d4c3ff285d7e4dd2f9b3cbe96b62c7502f64336f
To-steak <70811061+To-steak@users.noreply.github.com>
2024-09-12 10:17:25 +0900

    Claude
    Claude를 사용한 멀티 쓰레드 프로그래밍

---
+++ Mutex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+class MutexExample
+{
+    private static Mutex mutex = new Mutex();
+    private const int numThreads = 3;
+    private const int numIterations = 5;
+
+    static void Main()
+    {
+        for (int i = 0; i < numThreads; i++)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(WorkerThread));
+            thread.Start(i);
+        }
+
+        Console.ReadLine(); // 메인 쓰레드가 종료되지 않도록 대기
+    }
+
+    static void WorkerThread(object threadId)
+    {
+        for (int i = 0; i < numIterations; i++)
+        {
+            UseResource((int)threadId);
+        }
+    }
+
+    static void UseResource(int threadId)
+    {
+        Console.WriteLine($"Thread {threadId} waiting for the mutex.");
+        mutex.WaitOne();  // Mutex 획득 시도
+
+        try
+        {
+            Console.WriteLine($"Thread {threadId} has entered the critical section.");
+            Thread.Sleep(1000);  // 리소스 사용 시뮬레이션
+            Console.WriteLine($"Thread {threadId} is releasing the mutex.");
+        }
+        finally
+        {
+            mutex.ReleaseMutex();  // Mutex 해제
+        }
+    }
+}
---
+++ Semaphore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+class SemaphoreExample
+{
+    // 최대 3개의 쓰레드가 동시에 리소스에 접근할 수 있도록 세마포어 설정
+    private static SemaphoreSlim semaphore = new SemaphoreSlim(3);
+    private const int numThreads = 5;
+
+    static void Main()
+    {
+        for (int i = 0; i < numThreads; i++)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(WorkerThread));
+            thread.Start(i);
+        }
+
+        Console.ReadLine(); // 메인 쓰레드가 종료되지 않도록 대기
+    }
+
+    static void WorkerThread(object threadId)
+    {
+        Console.WriteLine($"Thread {threadId} is waiting to enter the semaphore.");
+        semaphore.Wait();
+
+        try
+        {
+            Console.WriteLine($"Thread {threadId} has entered the semaphore.");
+            // 리소스 사용 시뮬레이션
+            Thread.Sleep(new Random().Next(1000, 3000));
+        }
+        finally
+        {
+            Console.WriteLine($"Thread {threadId} is releasing the semaphore.");
+            semaphore.Release();
+        }
+    }
+}
